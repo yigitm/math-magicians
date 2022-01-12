759622e2fed0bd7b021773976a8241a386c3f218
Yigit Mersin <yigitmersin@gmail.com>
2022-01-12 04:12:18 +0300

    Add style to home & footer

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './components/Home';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
+import Footer from './components/Footer';
 import Error from './components/Error';
 
 const App = () => {
   return (
     <Router>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/calculator">Calculator</Link>
-        <Link to="/quote">Quote</Link>
+        <h1>Math Magicians</h1>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/calculator">Calculator</Link>
+          </li>
+          <li>
+            <Link to="/quote">Quote</Link>
+          </li>
+        </ul>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -19,6 +29,7 @@ const App = () => {
         <Route path="/quote" element={<Quote />} />
         <Route path="/*" element={<Error />} />
       </Routes>
+      <Footer />
     </Router>
   );
 };
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <h3>Microverse Inc.</h3>;
+    </footer>
+  );
+};
+
+export default Footer;
