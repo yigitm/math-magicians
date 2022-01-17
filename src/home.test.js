f5beae8619fa93cebd91e9d11222f300cfbc457d
Yigit Mersin <yigitmersin@gmail.com>
2022-01-17 19:10:03 +0300

    Update readme, deploy project

--- src/home.test.js
+++ src/home.test.js
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import Home from './components/Home';
 import React from 'react';
-import TestRenderer from 'react-test-renderer';
 
 describe('Home', () => {
   test('home', () => {
